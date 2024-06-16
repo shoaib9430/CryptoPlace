71fda1cc42f413b5fef142ccdca7f36b7e219e19
Shoaib Akhtar <shoaibakhtar9430@gmail.com>
2024-06-16 22:53:18 +0530

    first

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+        
+        <p className="footer">Copyright @ 2024, Cryptoplace - All Rights Reserved!</p>
+    </div>
+  )
+}
+
+export default Footer
